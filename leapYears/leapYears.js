cc0bd07914be390c91d16641cf2fbbd6f2c01492
catcecilia <phuongcatngo@gmail.com>
2020-10-21 19:23:45 -0400

    completed

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(questionedYear) {
+    if (questionedYear % 4 == 0 && questionedYear % 400 == 0) {
+        return true
+    }
+    else if (questionedYear % 4 == 0 && questionedYear % 100 != 0) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 module.exports = leapYears
