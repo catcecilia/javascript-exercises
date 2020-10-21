36107846da5e1d895ae4787cf23070e202cf2f90
catcecilia <phuongcatngo@gmail.com>
2020-10-21 11:50:29 -0400

    completed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let endSum = 0;
+    if ((typeof firstNum == 'string') || (typeof lastNum == 'string')) {
+        endSum = 'ERROR';
+        }
+    else if (isNaN(firstNum) || isNaN(lastNum)) {
+        endSum = 'ERROR';
+    }
+    else if (firstNum < 0 || lastNum < 0){
+        endSum = 'ERROR';
+    }
+    else if (firstNum > lastNum) {
+        for (i = firstNum; i >= lastNum; i--) {
+            endSum += i;
+        }
+    }
+        else {
+        
+        for (i = firstNum; i <= lastNum; i++) {
+            endSum += i;
+        }
+    }
+    return endSum
 }
 
 module.exports = sumAll
