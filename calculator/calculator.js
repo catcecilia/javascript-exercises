1eddda61988f8431b3e5fdcda6402b60b813c7af
catcecilia <phuongcatngo@gmail.com>
2020-10-22 11:33:50 -0400

    completed

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,46 @@
-function add () {
-	
+function add (firstNum, secondNum) {
+	let results = firstNum + secondNum;
+	return results
 }
 
-function subtract () {
-	
+function subtract (firstNum, secondNum) {
+	let results = firstNum - secondNum;
+	return results
 }
 
-function sum () {
-	
+function sum (anArray) {
+	let j = anArray.length;
+	let results = 0;
+	for (i = 1; i <= j; i++) {
+		results += anArray[i-1];
+	}
+	return results
 }
 
-function multiply () {
-	
+function multiply (anArray) {
+	let j = anArray.length;
+	let results = 1;
+	for (i = 1; i <= j; i++) {
+		results *= anArray[i-1];
+	}
+	return results
 }
 
-function power() {
-	
+function power(firstNum, secondNum) {
+	let results = Math.pow(firstNum, secondNum);
+	return results
 }
 
-function factorial() {
-	
+function factorial(firstNum) {
+	let results = 1;
+	if (firstNum == 0) {
+		results = 1;
+	} else if (firstNum > 0) {
+		for (i = 1; i <= firstNum; i++ ) {
+			results *= [i];
+		}
+	}
+	return results
 }
 
 module.exports = {
