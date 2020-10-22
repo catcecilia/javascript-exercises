34550b159053463affbdbc207e985816bcca930d
catcecilia <phuongcatngo@gmail.com>
2020-10-22 11:16:50 -0400

    complete

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(oldTemp) {
+  let newTemp = Math.round(((oldTemp-32)*5/9)*10)/10;
+  return newTemp
 }
 
-const ctof = function() {
-
+const ctof = function(oldTemp) {
+  let newTemp = Math.round(((oldTemp/5*9)+32)*10)/10; 
+  return newTemp
 }
 
 module.exports = {
